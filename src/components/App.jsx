5449a27e63d3bc6c43279badcf5d126c64b11de3
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-07-03 13:35:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Feedback } from './feedback/feedback';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
+        marginLeft: '50px',
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
